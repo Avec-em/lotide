d0a0dc563fcb49a21c9c9b330377a9739d74552b
Emily <emilymnicholas@gmail.com>
2020-04-28 02:29:14 +0000

    assterEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("👍🏼👍🏼👍🏼 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("☠️ ☠️ ☠️  Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 40);
