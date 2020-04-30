a062d8a2bc647460fa68206f745f6c6082c5ae9a
Emily <emilymnicholas@gmail.com>
2020-04-30 06:53:41 +0000

    adding findkeybyvalue.js to lotide

---
+++ findKeyByValue.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍🏼👍🏼👍🏼 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`☠️ ☠️ ☠️  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value){
+  let foundKey = "";
+  for (let i of Object.keys(object)) {
+    if (object[i] === value) {
+      return i
+    }
+  }
+  return object.i
+};
