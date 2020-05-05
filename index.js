358ecea0dbeeb8887fde3afec2430d901c37aa62
Emily <emilymnicholas@gmail.com>
2020-05-05 00:48:19 +0000

    adding index

---
+++ index.js
@@ -0,0 +1,11 @@
+const head = require('./head.js');
+const tail = require('./tail.js');
+const middle = require('./middle.js');
+
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle, 
+};
+
