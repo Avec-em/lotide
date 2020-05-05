b629ae0afa35ac228b48bfadd52293e3d3a09771
Emily <emilymnicholas@gmail.com>
2020-05-05 00:37:34 +0000

    adding mocha and chai to tail

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,15 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail')
+const tail = require('../tail');
+const assert = require('chai').assert;
 
-assertEqual(['hello', 'emily', 'hi'].length, 3); // original array should still have 3 elements!
-assertEqual(['hello', 'emily', 'bye', 'see-ya'].length, 4);
+// assertEqual(tail(['hello', 'emily', 'hi']).length, 2); // original array should still have 3 elements!
+// assertEqual(tail(['hello', 'emily', 'bye', 'see-ya']).length, 3);
+
+
+describe('tail', () => {
+  it ('returns ["emily", "hi"]', () => {
+    assert.deepEqual(tail(['hello', 'emily', 'hi']), ['emily', 'hi'])
+  });
+  it ('returns ["emily", "bye", "see-ya"]', () => {
+    assert.deepEqual(tail(['hello', 'emily', 'bye', 'see-ya']), ["emily", "bye", "see-ya"])
+  })
+});
